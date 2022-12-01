249fc7962f863d826d6f02e9790adda97358711a
northseadl <northseadl@outlook.com>
2022-12-01 15:05:11 +0800

    recovery

--- middleware/kratos/go.mod
+++ middleware/kratos/go.mod
@@ -5,6 +5,7 @@ go 1.19
 require (
 	github.com/go-kratos/kratos/v2 v2.5.3
 	github.com/northseadl/go-utils v0.0.8
+	github.com/pkg/errors v0.9.1
 )
 
 require (
--- middleware/kratos/recovery.go
+++ middleware/kratos/recovery.go
@@ -2,19 +2,26 @@ package kratos
 
 import (
 	"context"
-	"github.com/go-kratos/kratos/v2/errors"
+	kerrs "github.com/go-kratos/kratos/v2/errors"
 	"github.com/go-kratos/kratos/v2/log"
 	"github.com/go-kratos/kratos/v2/middleware"
+	"github.com/pkg/errors"
 )
 
-var ErrUnknownRequest = errors.InternalServer("UNKNOWN", "未知请求错误")
+var ErrUnknownRequest = kerrs.InternalServer("UNKNOWN", "未知请求错误")
 
 func RecoveryZeroMiddleware(logger log.Logger) func(handler middleware.Handler) middleware.Handler {
 	h := log.NewHelper(logger)
+	type stackTracer interface {
+		StackTrace() errors.StackTrace
+	}
 	return func(handler middleware.Handler) middleware.Handler {
 		return func(ctx context.Context, req interface{}) (reply interface{}, err error) {
 			defer func() {
 				if rerr := recover(); rerr != nil {
+					if _, ok := rerr.(stackTracer); !ok {
+						rerr = errors.WithStack(err)
+					}
 					h.WithContext(ctx).Errorw("req", req, "err", rerr)
 					err = ErrUnknownRequest
 				}
